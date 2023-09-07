a834a7ca5ede7bfbed51a5268d81182f9c2a1048
Ridley De Lange <ridleydelange1@gmail.com>
2023-09-07 23:29:33 +1000

    future_sounds_mp3

--- SpotifyImporter.py
+++ SpotifyImporter.py
@@ -1,1 +1,6 @@
-print("lol")
+from pytube import YouTube
+url1_future_sounds = "https://www.youtube.com/watch?v=dzQoliuqCfE"
+video = YouTube(url1_future_sounds)
+
+stream = video.streams.filter(only_audio=True).first()
+stream.download(filename=f"{video.title}.mp3")
